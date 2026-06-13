6b93cba61062a4c1d81db557923bf552a3e2163b
Lutosys <nickgaming271@gmail.com>
2026-06-13 15:07:15 -0400

    Refactor teleport handling and FFLAG logic

--- pfnorecoil.lua
+++ pfnorecoil.lua
@@ -10,25 +10,31 @@ if not getfflag or not setfflag or not hookfunction then
     return lplr:Kick("Unsupported Executor")
 end
 
+local queue_teleport = queue_on_teleport or (syn and syn.queue_on_teleport)
+
 local placeID = game.PlaceId
 local gameID = game.JobId
 
-local keeping = false
-
-if getfflag("DebugRunParallelLuaOnMainThread") == false then
-    setfflag("DebugRunParallelLuaOnMainThread", true)
-    keeping = true
-    lplr:Kick("Changed FFLAG rejoining the game!")
-    TeleportService:TeleportToPlaceInstance(placeID, gameID, lplr)
+if queue_teleport then
+    lplr.OnTeleport:Connect(function()
+        queue_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/Lutosys/opensrc/refs/heads/main/pfnorecoil'))()") 
+    end)
 end
 
-lplr.OnTeleport:Connect(function()
-    if keeping then
-        queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/Lutosys/opensrc/refs/heads/main/pfnorecoil'))()") 
-   end
-end)
+if getfflag("DebugRunParallelLuaOnMainThread") == false then
+    setfflag("DebugRunParallelLuaOnMainThread", "true")
+    
+    print("FFLAG changed, rejoining...")
+    
+    if gameID and gameID ~= "" then
+        TeleportService:TeleportToPlaceInstance(placeID, gameID, lplr)
+    else
+        TeleportService:Teleport(placeID, lplr)
+    end
+    return
+end
 
-for i,v in pairs(getgc(true)) do
+for i, v in pairs(getgc(true)) do
     if type(v) == "table" and rawget(v, "applyImpulse") then
         hookfunction(rawget(v, "applyImpulse"), function()
             return
