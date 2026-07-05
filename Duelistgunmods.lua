c6741f41d52b79d7fd3096c50adca44326bdf253
Lutosys <nickgaming271@gmail.com>
2026-07-04 20:05:56 -0400

    Update Duelistgunmods.lua

--- Duelistgunmods.lua
+++ Duelistgunmods.lua
@@ -1,1 +1,16 @@
+while task.wait() do
+    local char = game.Players.LocalPlayer.Character
+    if not char then 
+        continue
+    end
 
+    local tool = char:FindFirstChildOfClass("Tool")
+    if not tool then 
+        continue  
+    end
+
+    tool:SetAttribute("FireRate", 1000)
+    tool:SetAttribute("Automatic", true)
+    tool:SetAttribute("Recoil", 0)
+    tool:SetAttribute("Spread", 0)
+end
