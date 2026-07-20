22736831788d03c5938a0c831c2076c3cc3cbadb
Lutosys <nickgaming271@gmail.com>
2026-07-19 20:58:16 -0400

    Implement custom knockback velocity adjustment

---
+++ swordfightingpitvelocity.lua
@@ -0,0 +1,17 @@
+assert(hookfunction, game.Players.LocalPlayer:Kick("executor is not supported sorry"))
+
+local KnockbackVelocity = game:GetService("ReplicatedStorage").Remotes.Knockback
+
+local velocitypercent = 0
+
+local oldknockback
+oldknockback = hookfunction(getconnections(KnockbackVelocity.OnClientEvent)[1].Function, function(a1,a2)
+    local x,y,z = a2.X, a2.Y, a2.Z
+    x = x * velocitypercent/100
+    y = y * velocitypercent/100
+    z = z * velocitypercent/100
+
+    return oldknockback(a1,Vector3.new(x,y,z))
+end)
+
+print("success")
