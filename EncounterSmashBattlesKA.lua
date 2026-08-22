8a81b7dc553ab94e1f788dfe1d83ac060885fdab
Lutosys <nickgaming271@gmail.com>
2026-08-22 15:20:12 -0400

    Add utility module for player targeting and damage requests

---
+++ EncounterSmashBattlesKA.lua
@@ -0,0 +1,73 @@
+local utility = {
+    target = nil,
+    RunService = cloneref(game:GetService("RunService")),
+    Workspace = cloneref(game:GetService("Workspace")),
+    Players = cloneref(game:GetService("Players")),
+    ReplicatedStorage = cloneref(game:GetService("ReplicatedStorage"))
+}
+
+utility.getclosetplayer = function(self)
+    local closetdist = math.huge
+    local closet = nil
+
+    for key, plr in pairs(self.Players:GetPlayers()) do
+        if plr == self.LocalPlayer then continue end
+
+        local char = plr.Character
+        if not char then continue end   
+
+        if char:GetAttribute("Lobby") then
+            continue
+        end
+
+        local mychar = self.LocalPlayer.Character
+        if not mychar then continue end
+
+        if mychar:GetAttribute("Lobby") then
+            continue
+        end
+
+        mychar:MoveTo(Vector3.new(-0, 38, -2))
+
+        local hrp = char:FindFirstChild("HumanoidRootPart")
+        if not hrp then continue end    
+
+        local dist = self.LocalPlayer:DistanceFromCharacter(hrp.Position)
+        if dist < closetdist then
+            closetdist = dist 
+            closet = char
+        end
+    end
+
+    return closet
+end
+
+utility.Init = function(self)
+    self.LocalPlayer = self.Players.LocalPlayer
+    self.DamageRequest = self.ReplicatedStorage:WaitForChild("RE"):WaitForChild("DamageRequest")
+    self.runconn = self.RunService.RenderStepped:Connect(function(a0: number)
+        self.target = self:getclosetplayer()
+    end)
+
+    while true do
+        wait()
+        if self.target then
+            local head = self.target:FindFirstChild("Head")
+            if not head then
+                continue
+            end
+
+            local b = buffer.create(2)
+            buffer.writeu16(b, 0, math.random(105, 115))
+
+            self.DamageRequest:FireServer(
+                b,
+                workspace:GetServerTimeNow(),
+                CFrame.lookAt(head.CFrame.Position, Vector3.new(10000000,-100000,0)),
+                self.target
+            )
+        end
+    end
+end
+
+utility:Init()
