2246cfbcb7a7740160d1751196d44032efe1599b
Lutosys <nickgaming271@gmail.com>
2026-08-24 23:53:38 -0400

    Remove GetEnergy hooking and related error handling

--- EncounterSmashBattlesInfiniteDash.lua
+++ EncounterSmashBattlesInfiniteDash.lua
@@ -15,47 +15,22 @@ utility.Init = function(self)
         return self.LocalPlayer:Kick("executor doesnt have debug.getinfo")
     end
 
-    self.getenergygc, self.GetEnergy = pcall(function(...)
-        return filtergc("function", {Name = 'GetEnergy'}, true)    
-    end)
-
     self.addenergygc, self.AddEnergy = pcall(function(...)
         return filtergc("function", {Name = 'AddEnergy'}, true)    
     end)
 
-    if not self.GetEnergy then
-        return warn("failed to get GetEnergy func err: "..tostring(self.GetEnergy))
-    end
     if not self.AddEnergy then
         return warn("failed to get AddEnergy func err: "..tostring(self.AddEnergy))
     end
 
-    local _, getresult = pcall(function(...)
-        self.GetEnergyHook = hookfunction(self.GetEnergy, function(...)
-            local iscaller = debug.getinfo(2).source and debug.getinfo(2).source:find("SetUpDash")
-            if iscaller then
-                return 100
-            end
-            return self.GetEnergyHook(...)
-        end)
-    end)
-
     local _, addresult = pcall(function(...)
         self.AddEnergyHook = hookfunction(self.AddEnergy, function(...)
-            local iscaller = debug.getinfo(2).source and debug.getinfo(2).source:find("SetUpDash")
-            if iscaller then
-                return self.AddEnergyHook(0)
-            end
-            return self.AddEnergyHook(...)
+            return self.AddEnergyHook(0)
         end)
     end)
 
     if not self.AddEnergyHook  then
         return warn("failed to hook addenergy err: "..tostring(addresult))
-    end
-
-    if not self.GetEnergyHook then
-        return warn("failed to hook getenergy err: "..tostring(getresult))
     end
 
     return warn("success")
